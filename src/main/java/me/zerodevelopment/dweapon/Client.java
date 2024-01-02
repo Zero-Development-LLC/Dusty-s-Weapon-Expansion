cf7e8fefeee297cdd035e6c6b6b3bcc5715681de
CollectionStudios <bubbabinks@gmail.com>
2024-01-01 17:49:17 -0700

    Initial Commit

---
+++ src/main/java/me/zerodevelopment/dweapon/Client.java
@@ -0,0 +1,10 @@
+package me.zerodevelopment.dweapon;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Client implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
